e5ae7cb3bc833e5faa7f8cf6a33026c88047be64
John Fry <john@sixheads.com>
2024-03-14 16:31:11 +1100

    Fetch properties using our properties API route and sort in our components

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,8 +1,26 @@
 import Link from 'next/link';
-import properties from '@/properties.json';
+// import properties from '@/properties.json';
 import PropertyCard from '@/components/PropertyCard';
 
-const HomeProperties = () => {
+async function fetchProperties() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`
+    );
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch properties');
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const HomeProperties = async () => {
+  const properties = await fetchProperties();
+
   const recentProperties = properties
     .sort(() => Math.random() - Math.random())
     .slice(0, 3);
