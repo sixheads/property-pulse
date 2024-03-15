c4d9f1ff1d015cea5743ecec0df7d8934bcf705f
John Fry <john@sixheads.com>
2024-03-15 20:39:02 +1100

    Fix the image reference

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -6,7 +6,7 @@ const PropertyHeaderImage = ({ image }) => {
       <div className="container-xl m-auto">
         <div className="grid grid-cols-1">
           <Image
-            src={image}
+            src={`/images/properties/${image}`}
             alt=""
             className="object-cover h-[400px] w-full"
             width={0}
